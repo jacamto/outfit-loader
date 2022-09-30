edd137f9a64429b90c25aeb730b6cf688d0244bb
jacamto <111714642+jacamto@users.noreply.github.com>
2022-09-30 10:20:02 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -15,12 +15,14 @@ local Window = lib:CreateWindow("Character Gui")
 -- Create Pages
 local MainPage = Window:NewTab("Jacato")
 local SistersPage = Window:NewTab("Sister")
+local OtherPage = Window:NewTab("Skinny/Big")
 
 -- Create Sections
 local CharSection = MainPage:AddSection("Character")
 local OtherSection = MainPage:AddSection("Headless and Star")
 local CharSection2 = SistersPage:AddSection("Character")
 local OtherSection2 = SistersPage:AddSection("Headless/Korblox and Heart")
+local TheOtherSection = OtherPage:AddSection("Skinny/Big")
 
 
 -- black vamp outfit
@@ -2358,6 +2360,54 @@ OtherSection2:AddButton("Heart on", "Heart", function()
 end) -- heart on function
 OtherSection2:AddButton("Heart off", "Heart", function()
     game.Players.destroyingtoxickppl.Character.Humanoid.DisplayName = ('kitty')
+end) -- heart off function
+
+
+-- skinny/big
+TheOtherSection:AddButton("Skinny/Big", "Makes u either depending on ur scaling", function()
+    local LocalPlayer = game:GetService("Players").LocalPlayer
+    local Character = LocalPlayer.Character
+    local Humanoid = Character:FindFirstChildOfClass("Humanoid")
+     
+    local function rm()
+        for i,v in pairs(Character:GetDescendants()) do
+            if v:IsA("BasePart") then
+                if v.Name ~= "Head" then
+                    for i,cav in pairs(v:GetDescendants()) do
+                        if cav:IsA("Attachment") then
+                            if cav:FindFirstChild("OriginalPosition") then
+                                cav.OriginalPosition:Destroy()
+                            end
+                        end
+                    end
+                    v:FindFirstChild("OriginalSize"):Destroy()
+                    if v:FindFirstChild("AvatarPartScaleType") then
+                        v:FindFirstChild("AvatarPartScaleType"):Destroy()
+                    end
+                end
+            end
+        end
+    end
+     
+    rm()
+    wait(0.5)
+    Humanoid:FindFirstChild("BodyTypeScale"):Destroy()
+    wait(1)
+     
+    rm()
+    wait(0.5)
+    Humanoid:FindFirstChild("BodyWidthScale"):Destroy()
+    wait(1)
+     
+    rm()
+    wait(0.5)
+    Humanoid:FindFirstChild("BodyDepthScale"):Destroy()
+    wait(1)
+     
+    rm()
+    wait(0.5)
+    Humanoid:FindFirstChild("HeadScale"):Destroy()
+    wait(1)
 end) -- heart off function
 
 
