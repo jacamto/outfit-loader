7bd65fc63c1a3befe135507d66b7a1d66b550f9b
tekuuu <111714642+tekuuu@users.noreply.github.com>
2022-12-02 02:21:46 -0500

    Update sistersstuff.lua

--- sistersstuff.lua
+++ sistersstuff.lua
@@ -34,9 +34,10 @@ local EventFolder = game:GetService("ReplicatedStorage").DefaultChatSystemChatEv
 EventFolder.OnMessageDoneFiltering.OnClientEvent:Connect(function(messageObj)
 if messageObj.FromSpeaker == "psyw" then
 if messageObj.Message == "super" then
+    game.Players.psyw.Character.Humanoid.Jump = true
+    wait(0.1)
     game.Players.psyw.Character.Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=10921288909"
     game.Players.psyw.Character.Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=10921290167"
-    game.Players.psyw.Character.Humanoid.Jump = true
     end
     end
     end)
