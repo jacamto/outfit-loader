248f78274472877f6ea4405bf269a3f3060c54eb
tekuuu <111714642+tekuuu@users.noreply.github.com>
2022-12-06 22:13:53 -0500

    Update moe.lua

--- moe.lua
+++ moe.lua
@@ -14,7 +14,7 @@ local name = "moe"         -- orestias | moe | farzad
 local sister2 = "1"
 local noel1 = "1"
 local fit = "1"
-local loader = "1"
+local loader = "0"
 local startname = name
 local horns = b
 
