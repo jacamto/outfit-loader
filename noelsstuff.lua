6fd7afe4907b8cd7c1c1393547939ea995e8b3e9
tekuuu <111714642+tekuuu@users.noreply.github.com>
2022-12-18 22:46:46 -0500

    Update noelsstuff.lua

--- noelsstuff.lua
+++ noelsstuff.lua
@@ -3,6 +3,7 @@
 
 local fit3 = "1"
 local princessAnim = "m"
+local headless = "hl"
 
 
 
@@ -22,6 +23,16 @@ game.Players.istolewillshoodie.Character.Humanoid.Jump = true
 wait(0.1)
 game.Players.istolewillshoodie.Character.Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=941003647"
 game.Players.istolewillshoodie.Character.Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=941013098"
+end
+end
+end)
+	
+local EventFolder = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents
+
+EventFolder.OnMessageDoneFiltering.OnClientEvent:Connect(function(messageObj)
+if messageObj.FromSpeaker == "istolewillshoodie" then
+if messageObj.Message == headless then
+game.Players.istolewillshoodie.Character.Head.MeshId = "rbxassetid://6686307858"
 end
 end
 end)
