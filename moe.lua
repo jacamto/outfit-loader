4d0b05ff092030d4ec9307919c230e3b58f78e67
tekuuu <111714642+tekuuu@users.noreply.github.com>
2022-12-18 22:44:57 -0500

    Update moe.lua

--- moe.lua
+++ moe.lua
@@ -34,6 +34,7 @@ local name = "moe"         -- orestias | moe | farzad | kittysp1t
 
 local sister2 = "1"
 local rushstuff1 = "1"
+local noel1 = "1"
 local fit = "1"
 local startname = name
 local horns = b
@@ -74,6 +75,10 @@ end
 
 if rushstuff1 == "1" then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/tekuuu/outfit-loader/main/rushstuff.lua"))()
+end
+
+if noel1 == "1" then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tekuuu/outfit-loader/main/noelsstuff.lua"))()
 end
 
 if fit == "1" then
