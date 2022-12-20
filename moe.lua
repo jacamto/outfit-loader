b865e1c1aecd30abe15236d9e11dc7746bcea721
tekuuu <111714642+tekuuu@users.noreply.github.com>
2022-12-20 17:16:13 -0500

    Update moe.lua

--- moe.lua
+++ moe.lua
@@ -1,5 +1,5 @@
 -- v.0.5.4 (everybody knows that im a good girl officer ❤️)
-wait (10)
+wait(10)
 local notification = true
 if notification == true then
 local notifLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/tekuuu/outfit-loader/main/notiflib.lua"))()
