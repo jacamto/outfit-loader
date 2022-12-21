8258d7ded899ad197bcbda5a55b39eaf53a955e6
tekuuu <111714642+tekuuu@users.noreply.github.com>
2022-12-21 16:33:13 -0500

    Update moe.lua

--- moe.lua
+++ moe.lua
@@ -3591,6 +3591,13 @@ notifLib:CreateDefaultNotif({
  Title = "hey me..", 
  Text = "                                                   Script Loaded", Duration = 10
 })
+if game.PlaceId == 6516141723 then  -- doors :)
+notifLib:CreateDefaultNotif({
+ TweenSpeed = 1.2,
+ Title = "CHECK YOUR CLIPBOARD!!", 
+ Text = "                DOORS SCRIPT DO CTRL V IN A SYNAPSE TAB", Duration = 10
+})		
+end
 
 elseif game.Players.LocalPlayer.Name == "psyw" then
 local notifLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/tekuuu/outfit-loader/main/notiflib.lua"))()
@@ -3600,6 +3607,13 @@ notifLib:CreateDefaultNotif({
  Title = "LY SIS ❤️", 
  Text = "                                                   Script Loaded", Duration = 10
 })
+if game.PlaceId == 6516141723 then  -- doors :)
+notifLib:CreateDefaultNotif({
+ TweenSpeed = 1.2,
+ Title = "CHECK YOUR CLIPBOARD!!", 
+ Text = "                DOORS SCRIPT DO CTRL V IN A SYNAPSE TAB", Duration = 10
+})		
+end
 		
 elseif game.Players.LocalPlayer.Name == "istolewillshoodie" then
 local notifLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/tekuuu/outfit-loader/main/notiflib.lua"))()
@@ -3621,6 +3635,13 @@ notifLib:CreateDefaultNotif({
  "          and             hll = headless for R6",
  Duration = 20
 })
+if game.PlaceId == 6516141723 then  -- doors :)
+notifLib:CreateDefaultNotif({
+ TweenSpeed = 1.2,
+ Title = "CHECK YOUR CLIPBOARD!!", 
+ Text = "                DOORS SCRIPT DO CTRL V IN A SYNAPSE TAB", Duration = 10
+})		
+end
 		
 elseif game.Players.LocalPlayer.Name == "DreamingRush" then
 local notifLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/tekuuu/outfit-loader/main/notiflib.lua"))()
